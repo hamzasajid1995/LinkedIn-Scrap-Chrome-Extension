fa12f4cfbe6df2e5cfdf7cc4ca36e732397d00ea
hamza sajid <hamzasajid424@gmail.com>
2021-03-21 23:52:30 +0500

    scraper logic

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,18 +1,62 @@
 /* global chrome */
 
 import React from "react";
+import { Component } from "react";
 
-import { AppWrapper } from "./style";
+class index extends Component {
+  constructor() {
+    super();
+    this.state = { details: {} };
+  }
 
-const handleClick = () => {
-  chrome.tabs.query({ currentWindow: true, active: true }, ([activeTab]) => {
-    chrome.tabs.sendMessage(activeTab.id, { message: "start" });
-  });
-};
+  handleClick = () => {
+    chrome.tabs.query({ currentWindow: true, active: true }, ([activeTab]) => {
+      chrome.tabs.sendMessage(activeTab.id, { message: "start" });
+      chrome.tabs.executeScript(
+        activeTab.id,
+        {
+          code:
+            `(` +
+            function query() {
+              let executedQueries = {};
+              const queries = {
+                name: "ul.pv-top-card--list li.inline.t-24.t-black.t-normal.break-words",
+                title: "h2.mt1.t-18.t-black.t-normal.break-words",
+                location: "li.t-16.t-black.t-normal.inline-block",
+              };
+              for (let i in queries) {
+                try {
+                  executedQueries[i] = document.querySelector(queries[i]).innerText;
+                } catch (error) {
+                  executedQueries[i] = "error: " + JSON.stringify(error) + " query: " + queries[i];
+                }
+              }
+              return executedQueries;
+            } +
+            `)()`,
+        },
+        (result) => {
+          this.setState({ details: result[0] });
+        }
+      );
+    });
+  };
+  render() {
+    return (
+      <div>
+        <button onClick={this.handleClick}>Scrap</button>
+        <ul>
+          {Object.keys(this.state.details).map((item, index) => (
+            <li key={index}>
+              <b>{item}</b>
+              {": "}
+              <b>{this.state.details[item]}</b>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
 
-export default () => (
-  <AppWrapper>
-    I am the popup from the Chrome Extension template!{" "}
-    <button onClick={handleClick}>Click me!</button>
-  </AppWrapper>
-);
+export default index;
--- webpack.config.js
+++ webpack.config.js
@@ -10,20 +10,14 @@ const HTMLWebpackPluginConfig = new HTMLWebpackPlugin({
   template: path.join(__dirname, "/src/index.html"),
   filename: "index.html",
   inject: "body",
-  chunks: ["popup"]
+  chunks: ["popup"],
 });
 
 const DefinePluginConfig = new webpack.DefinePlugin({
-  "process.env.NODE_ENV": JSON.stringify("production")
+  "process.env.NODE_ENV": JSON.stringify("production"),
 });
 
-const copyWebpackPlugin = new copyWebpack([
-  { from: "./src/manifest.json" },
-  { from: "./src/icon-16.png" },
-  { from: "./src/icon-128.png" },
-  { from: "./src/icon-19.png" },
-  { from: "./src/icon-38.png" }
-]);
+const copyWebpackPlugin = new copyWebpack([{ from: "./src/manifest.json" }, { from: "./src/icon.png" }]);
 
 module.exports = {
   devServer: {
@@ -31,19 +25,19 @@ module.exports = {
     port: "3000",
     hot: true,
     headers: {
-      "Access-Control-Allow-Origin": "*"
+      "Access-Control-Allow-Origin": "*",
     },
-    historyApiFallback: true
+    historyApiFallback: true,
   },
 
   entry: {
     popup: "./src/popup.js",
-    content: "./src/content.js"
+    content: "./src/content.js",
   },
 
   output: {
     filename: "[name].js",
-    path: path.join(__dirname, "/build")
+    path: path.join(__dirname, "/build"),
   },
 
   module: {
@@ -53,32 +47,27 @@ module.exports = {
         exclude: /node_modules/,
         use: [
           {
-            loader: "babel-loader"
-          }
-        ]
+            loader: "babel-loader",
+          },
+        ],
       },
       {
         test: /\.(jpe?g|png|gif|svg)$/i,
         loader: "url-loader",
         options: {
-          limit: 10000
-        }
-      }
-    ]
+          limit: 10000,
+        },
+      },
+    ],
   },
 
   resolve: {
-    extensions: [".js", ".png", ".css", ".scss"]
+    extensions: [".js", ".png", ".css", ".scss"],
   },
 
   mode: dev ? "development" : "production",
 
   plugins: dev
-    ? [
-        HTMLWebpackPluginConfig,
-        new webpack.HotModuleReplacementPlugin(),
-        new ChromeExtensionReloader(),
-        copyWebpackPlugin
-      ]
-    : [HTMLWebpackPluginConfig, DefinePluginConfig, copyWebpackPlugin]
+    ? [HTMLWebpackPluginConfig, new webpack.HotModuleReplacementPlugin(), new ChromeExtensionReloader(), copyWebpackPlugin]
+    : [HTMLWebpackPluginConfig, DefinePluginConfig, copyWebpackPlugin],
 };
